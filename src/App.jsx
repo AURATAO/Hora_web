c7b75af3e1358cfcc529451f64a96e07e4f397a9
Aura Tao <auratao.model@gmail.com>
2025-06-30 13:02:41 +0200

    fix: move index.html to Public file

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Camera } from "lucide-react";
+
+function App() {
+  return (
+    <>
+      <div className="text-3xl font-bold underline text-red-50">
+        Hello Tailwind + React + Vite!
+      </div>
+      <Camera color="red" size={48} />
+    </>
+  );
+}
+
+export default App;
