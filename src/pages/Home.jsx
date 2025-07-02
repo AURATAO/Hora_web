6516792c1ee9d658b2a6b44671c41a4ae6148265
Aura Tao <auratao.model@gmail.com>
2025-07-02 16:22:03 +0200

    feat: add section 3 how it works, section 4 partners, section 5 testimonials slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
-
-
+import { MoveDownRight } from 'lucide-react';
+import {MoveDownLeft} from 'lucide-react';
+import TestimonialsSlider from './components/TestimonialsSlider.jsx';
 
 export default function Home() {
   return (
   <>
-
   <header className="flex items-center justify-between py-4 px-[50px] h-[72px] w-full fixed shadow-[0_12px_22px_0_rgba(0,0,0,0.08)] z-40 bg-primary/20 " >
     <div className="flex items-center justify-between w-full max-w-7xl mx-auto">
     <nav className="flex space-x-5">
@@ -34,13 +34,13 @@ export default function Home() {
     {/* Section 1 */}
     <div className="py-[100px] w-full mx-auto flex flex-col items-center justify-center">
       <h2 className="font-heading text-6xl text-left font-semibold mb-4 text-primary p-4">What is Hora?</h2>
-      <p className="text-lg mb-8 max-w-2xl text-center space-y-3 ">
+      <div className="text-lg mb-8 max-w-2xl text-center space-y-3 ">
         <p className=" text-primary ">Life’s busy. We get your time back.</p>
 
         <p  className=" text-primary ">Find someone kind to handle the little things — whether it’s walking your dog, sitting with your mom at the clinic, or sharing a meal so you don’t eat alone. </p>
 
        <p  className=" text-primary pb-4"> Your day stays lighter. <br/> Your mind stays clearer.<br/> Someone else shows up, so you can keep moving.</p>
-      </p>
+      </div>
       <button className="py-9">
       <a href="/mission" className="text-xl px-5 py-2 bg-secondary text-white rounded-full hover:bg-accent hover:border-secondary hover:border-1 hover:text-secondary transition-colors duration-300">
         How we do it
@@ -56,9 +56,9 @@ export default function Home() {
           </p>
         </div>
 
-        <div className="flex flex-col lg:flex-row items-start justify-between gap-[32px] w-full pt-[64px]">
+        <div className="flex flex-col lg:flex-row items-start justify-between gap-[32px] w-full pt-[32px] pb-2 max-w-7xl">
           <div className="flex flex-col items-start">
-            <h2 className="text-[200px] font-semibold text-accent leading-none">01</h2>
+            <h2 className="text-[200px] font-semibold text-accent leading-none font-heading pb-[64px]">01</h2>
             <div className="flex flex-col items-start pt-2">
               <h3 className="text-3xl font-semibold text-accent pb-4 font-heading">
                 Your safety, our priority.
@@ -70,7 +70,7 @@ export default function Home() {
           </div>
 
           <div className="flex flex-col items-start">
-            <h2 className="text-[200px] font-semibold text-accent leading-none">02</h2>
+            <h2 className="text-[200px] font-semibold text-accent leading-none font-heading pb-[64px]">02</h2>
             <div className="flex flex-col items-start pt-2">
               <h3 className="text-3xl font-semibold text-accent pb-4 font-heading">
                 Your Money, Always Protected
@@ -82,7 +82,7 @@ export default function Home() {
           </div>
 
           <div className="flex flex-col items-start">
-            <h2 className="text-[200px] font-semibold text-accent leading-none">03</h2>
+            <h2 className="text-[200px] font-semibold text-accent leading-none font-heading pb-[64px]">03</h2>
             <div className="flex flex-col items-start pt-2">
               <h3 className="text-3xl font-semibold text-accent pb-4 font-heading">
                 Your every minute matters
@@ -94,6 +94,103 @@ export default function Home() {
           </div>
         </div>
       </div>
+    </div>
+    {/* Section 2 */}
+    <div className=" bg-accent w-full mx-auto flex flex-col items-center justify-center max-w-7xl">
+      <label className="label flex self-end py-[48px]">
+        <div className="toggle">
+          <input className="toggle-state" type="checkbox" name="check" value="check" />
+          <div className="indicator"></div>
+        </div>
+      </label>
+    {/* Section requester */}
+    <div className=" w-full mx-auto items-center justify-center pb-[32px] hidden">
+    <div className="bg-[url(/img/section_3_1.jpeg)] w-full h-[400px] md:w-full md:h-lvh bg-cover bg-center transition-transform duration-500 hover:scale-105"/>
+      <div className="w-full max-w-3xl mx-auto px-6 py-16 flex flex-col items-center justify-center">
+        {/* Step 1 */}
+        <div className="flex flex-col items-start pb-12">
+          <h2 className="text-[80px] font-semibold text-primary leading-none mb-4">01</h2>
+          <h3 className="text-2xl font-heading font-semibold text-primary mb-4">Establish your profile</h3>
+          <p className="text-lg text-primary max-w-lg">
+            Join our exclusive circle of verified members. Your trust is our highest standard.
+          </p>
+        </div>
+       <MoveDownRight className='text-secondary transition-transform duration-500 hover:scale-200 ' size={32} />
+
+        {/* Step 2 */}
+        <div className="flex flex-col items-end pb-12">
+          <h2 className="text-[80px] font-semibold text-primary leading-none mb-4">02</h2>
+          <h3 className="text-2xl font-heading font-semibold text-primary mb-4">Post your needs</h3>
+          <p className="text-lg text-primary max-w-lg text-right">
+            Specify your requirements with precision. We’ll connect you with exceptional supporters who meet your expectations.
+          </p>
+        </div>
+        <MoveDownLeft className='text-secondary transition-transform duration-500 hover:scale-200 ' size={32} />
+
+        {/* Step 3 */}
+        <div className="flex flex-col items-start">
+          <h2 className="text-[80px] font-semibold text-primary leading-none mb-4">03</h2>
+          <h3 className="text-2xl font-heading font-semibold text-primary mb-4">Complete with confidence</h3>
+          <p className="text-lg text-primary max-w-lg">
+            Approve the service only when fully satisfied. Settle payments seamlessly, with clarity and grace.
+          </p>
+        </div>
+      </div>
+
+    </div>
+    {/* Section supporter */}
+    <div className=" w-full mx-auto flex items-center justify-center pb-[32px]  ">
+      <div className="w-full max-w-3xl mx-auto px-6 py-16 flex flex-col items-center justify-center ">
+        {/* Step 1 */}
+        <div className="flex flex-col items-start pb-12">
+          <h2 className="text-[80px] font-semibold text-primary leading-none mb-4">01</h2>
+          <h3 className="text-2xl font-heading font-semibold text-primary mb-4">Build your trusted profile</h3>
+          <p className="text-lg text-primary max-w-lg">
+           Register and complete verification to join a community that values discretion and quality.
+          </p>
+        </div>
+
+        {/* Step 2 */}
+        <div className="flex flex-col items-start pb-12">
+          <h2 className="text-[80px] font-semibold text-primary leading-none mb-4">02</h2>
+          <h3 className="text-2xl font-heading font-semibold text-primary mb-4">Choose or Accept tasks that suit you</h3>
+          <p className="text-lg text-primary max-w-lg ">
+            Browse requests and choose engagements aligned with your time and expertise.
+          </p>
+        </div>
+
+        {/* Step 3 */}
+        <div className="flex flex-col items-start">
+          <h2 className="text-[80px] font-semibold text-primary leading-none mb-4">03</h2>
+          <h3 className="text-2xl font-heading font-semibold text-primary mb-4">Deliver and Earn with grace</h3>
+          <p className="text-lg text-primary max-w-lg">
+            Complete the tasks with confidence, turning free time into real value — knowing your earnings are secured and your reputation elevated.
+          </p>
+        </div>
+      </div>
+      <div className="bg-[url(/img/section_3_2.jpeg)] w-full h-[400px] md:w-full md:h-lvh bg-cover bg-center transition-transform duration-500 hover:scale-105" />
+    </div>
+    </div>
+    {/* Section 3 */}
+    <div className=' w-full mx-auto flex flex-col items-center justify-center bg-neutralbg'>
+    <div className="py-[32px] w-full mx-auto max-w-7xl flex flex-wrap items-center justify-center gap-x-32 ">
+      <img src="/img/logo/Forbes.png" alt="Forbes" className="w-24 opacity-60"/>
+      <img src="/img/logo/Stripe.png" alt="Stripe" className="w-24 opacity-60"/>
+      <img src="/img/logo/Visa.png" alt="Visa" className="w-24 opacity-60"/>
+      <img src="/img/logo/Vogue.png" alt="Vogue" className="w-24 opacity-60"/>
+      <img src="/img/logo/Forbes.png" alt="Forbes" className="w-24 opacity-60"/>
+    </div>
+    </div>
+    {/* Section 4 */}
+    <div className='w-full mx-auto flex flex-col items-center justify-center bg-primary py-[32px]'>
+  <div className="w-full mx-auto max-w-7xl flex flex-col items-start justify-center gap-32">
+    <p className='text-secondary text-xl'>What our users say</p>
+    <TestimonialsSlider />
+  </div>
+    </div>
+    {/* Section 5 */}
+    <div className='w-full h-[300px] mx-auto flex flex-col items-center justify-center bg-[url(/img/section_5_1.JPG)] bg-cotain bg-center bg-no-repeat py-[64px]'>
+
     </div>
   </main>
     
---
+++ src/pages/components/TestimonialsSlider.jsx
@@ -0,0 +1,120 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, EffectFade } from 'swiper/modules';
+import 'swiper/css/effect-fade';
+
+export default function TestimonialsSlider() {
+  return (
+    <div className="py-16 bg-primary w-full">
+      <Swiper
+           modules={[Autoplay, EffectFade]}
+        effect="fade"
+        fadeEffect={{ crossFade: true }}
+        slidesPerView={1}
+        loop={true}
+        autoplay={{ delay: 5000 }}
+        speed={1000}
+      >
+        <SwiperSlide>
+          <blockquote className="max-w-3xl mr-auto text-left px-6">
+            <div className="text-accent mb-6">
+              {/* SVG quote icon */}
+           <svg viewBox="0 0 35 26" fill="none" xmlns="http://www.w3.org/2000/svg" className="mr-auto h-5 text-secondary">
+            <path d="M25 11.749C25.4348 6.96234 29.0217 3.91631 35 2.93723V0C24.4565 0.761503 
+                    18.6957 7.17991 18.6957 15.8828C18.6957 21.7573 21.7391 26 26.9565 26C31.3043 
+                    26 34.6739 23.0628 34.6739 18.6025C34.6739 14.6862 32.1739 12.4017 29.0217 
+                    11.749H25ZM6.19565 11.749C6.73913 6.96234 10.2174 3.91631 16.3043 2.93723V0C5.76087 
+                    0.761503 0 7.17991 0 15.8828C0 21.7573 3.04348 26 8.26087 26C12.5 26 15.9783 
+                    23.0628 15.9783 18.6025C15.9783 14.6862 13.4783 12.4017 10.3261 11.749H6.19565Z" 
+                    fill="currentColor" />
+            </svg>
+            </div>
+            <p className="text-2xl md:text-3xl font-serif text-accent mb-6">
+             I hired someone on Hora to help move my new dresser — saved a ton on delivery fees. Super chill experience, he even helped me set it up.
+            </p>
+            <span className="text-lg font-sans block text-accent">Sofia R.</span>
+          </blockquote>
+        </SwiperSlide>
+        <SwiperSlide>
+          <blockquote className="max-w-3xl mr-auto text-left px-6">
+            <div className="text-accent mb-6">
+              {/* SVG quote icon */}
+           <svg viewBox="0 0 35 26" fill="none" xmlns="http://www.w3.org/2000/svg" className="mr-auto h-5 text-secondary">
+            <path d="M25 11.749C25.4348 6.96234 29.0217 3.91631 35 2.93723V0C24.4565 0.761503 
+                    18.6957 7.17991 18.6957 15.8828C18.6957 21.7573 21.7391 26 26.9565 26C31.3043 
+                    26 34.6739 23.0628 34.6739 18.6025C34.6739 14.6862 32.1739 12.4017 29.0217 
+                    11.749H25ZM6.19565 11.749C6.73913 6.96234 10.2174 3.91631 16.3043 2.93723V0C5.76087 
+                    0.761503 0 7.17991 0 15.8828C0 21.7573 3.04348 26 8.26087 26C12.5 26 15.9783 
+                    23.0628 15.9783 18.6025C15.9783 14.6862 13.4783 12.4017 10.3261 11.749H6.19565Z" 
+                    fill="currentColor" />
+            </svg>
+            </div>
+            <p className="text-2xl md:text-3xl font-serif text-accent mb-6">
+              I love how easy it is to find someone reliable on Hora.<br/>
+              I posted my request in the morning and by lunch, everything was sorted. So simple!
+            </p>
+            <span className="text-lg font-sans block text-accent">Emily T.</span>
+          </blockquote>
+        </SwiperSlide>
+        <SwiperSlide>
+          <blockquote className="max-w-3xl mr-auto text-left px-6">
+            <div className="text-accent mb-6">
+              {/* SVG quote icon */}
+           <svg viewBox="0 0 35 26" fill="none" xmlns="http://www.w3.org/2000/svg" className="mr-auto h-5 text-secondary">
+            <path d="M25 11.749C25.4348 6.96234 29.0217 3.91631 35 2.93723V0C24.4565 0.761503 
+                    18.6957 7.17991 18.6957 15.8828C18.6957 21.7573 21.7391 26 26.9565 26C31.3043 
+                    26 34.6739 23.0628 34.6739 18.6025C34.6739 14.6862 32.1739 12.4017 29.0217 
+                    11.749H25ZM6.19565 11.749C6.73913 6.96234 10.2174 3.91631 16.3043 2.93723V0C5.76087 
+                    0.761503 0 7.17991 0 15.8828C0 21.7573 3.04348 26 8.26087 26C12.5 26 15.9783 
+                    23.0628 15.9783 18.6025C15.9783 14.6862 13.4783 12.4017 10.3261 11.749H6.19565Z" 
+                    fill="currentColor" />
+            </svg>
+            </div>
+            <p className="text-2xl md:text-3xl font-serif text-accent mb-6">
+              I’d just gotten out of a long relationship and needed someone to talk to. Ended up chatting for three hours over coffee. Zero pressure, just good vibes.<br/>
+              Highly recommeded if you need a friendly ear.
+            </p>
+            <span className="text-lg font-sans block text-accent"> Carlos M.</span>
+          </blockquote>
+        </SwiperSlide>
+        <SwiperSlide>
+          <blockquote className="max-w-3xl mr-auto text-left px-6">
+            <div className="text-accent mb-6">
+              {/* SVG quote icon */}
+           <svg viewBox="0 0 35 26" fill="none" xmlns="http://www.w3.org/2000/svg" className="mr-auto h-5 text-secondary">
+            <path d="M25 11.749C25.4348 6.96234 29.0217 3.91631 35 2.93723V0C24.4565 0.761503 
+                    18.6957 7.17991 18.6957 15.8828C18.6957 21.7573 21.7391 26 26.9565 26C31.3043 
+                    26 34.6739 23.0628 34.6739 18.6025C34.6739 14.6862 32.1739 12.4017 29.0217 
+                    11.749H25ZM6.19565 11.749C6.73913 6.96234 10.2174 3.91631 16.3043 2.93723V0C5.76087 
+                    0.761503 0 7.17991 0 15.8828C0 21.7573 3.04348 26 8.26087 26C12.5 26 15.9783 
+                    23.0628 15.9783 18.6025C15.9783 14.6862 13.4783 12.4017 10.3261 11.749H6.19565Z" 
+                    fill="currentColor" />
+            </svg>
+            </div>
+            <p className="text-2xl md:text-3xl font-serif text-accent mb-6">
+              I was nervous booking someone just to have dinner with me, but everyone’s been so kind and genuine. Already planning to have someone join me for a movie next time. </p>
+            <span className="text-lg font-sans block text-accent"> Lina K.</span>
+          </blockquote>
+        </SwiperSlide>
+        <SwiperSlide>
+          <blockquote className="max-w-3xl mr-auto text-left px-6">
+            <div className="text-accent mb-6">
+              {/* SVG quote icon */}
+           <svg viewBox="0 0 35 26" fill="none" xmlns="http://www.w3.org/2000/svg" className="mr-auto h-5 text-secondary">
+            <path d="M25 11.749C25.4348 6.96234 29.0217 3.91631 35 2.93723V0C24.4565 0.761503 
+                    18.6957 7.17991 18.6957 15.8828C18.6957 21.7573 21.7391 26 26.9565 26C31.3043 
+                    26 34.6739 23.0628 34.6739 18.6025C34.6739 14.6862 32.1739 12.4017 29.0217 
+                    11.749H25ZM6.19565 11.749C6.73913 6.96234 10.2174 3.91631 16.3043 2.93723V0C5.76087 
+                    0.761503 0 7.17991 0 15.8828C0 21.7573 3.04348 26 8.26087 26C12.5 26 15.9783 
+                    23.0628 15.9783 18.6025C15.9783 14.6862 13.4783 12.4017 10.3261 11.749H6.19565Z" 
+                    fill="currentColor" />
+            </svg>
+            </div>
+            <p className="text-2xl md:text-3xl font-serif text-accent mb-6">
+              I have a car, so on weekends I pick up small gigs through Hora. Easy way to make extra cash, and the people I’ve met have all been really friendly.  </p>
+            <span className="text-lg font-sans block text-accent"> Davide T.</span>
+          </blockquote>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
